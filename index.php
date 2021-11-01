c11c71e6b21a8ba8bab207eb034894140c14fed0
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-11-01 22:54:39 +0100

    get and set functions for each class

--- index.php
+++ index.php
@@ -1,67 +1,122 @@
 <?php 
 
-    // // proprietà, cioè una variabile
-    // public $nome="pippo";
-
-    // // metodo, cioè una funzione
-    // public function saluto(){
-    //    echo "ciao da ".$this->nome;
-    // }
-
     class User {
 
-        public $name;
-        public $mail;
-        public $password;
+        protected $name;
+        protected $mail;
+        protected $password;
 
         public function __construct($name, $mail, $password) {
             $this -> name = $name;
             $this -> mail = $mail;
             $this -> password = $password;
         }
+
+        public function getName() {
+            return $this -> name;
+        }
+
+        public function setName($name) {
+            return $this -> name = $name;
+        }
+
+        public function getMail() {
+            return $this -> mail;
+        }
+
+        public function setMail($mail) {
+            return $this -> mail = $mail;
+        }
+
+        public function getPassword() {
+            return $this -> password;
+        }
+
+        public function setPassword($password) {
+            return $this -> password = $password;
+        }
     };
 
     class Account {
 
-        public $accountId;
-        public $pointsNumber;
-        public $purchasesRecords;
+        protected $accountId;
+        protected $pointsNumber;
+        protected $purchasesRecord;
 
         public function __construct($accountId, $pointsNumber, $purchasesRecords) {
             $this -> accountId = $accountId;
             $this -> pointsNumber = $pointsNumber;
             $this -> purchasesRecords = $purchasesRecords;
+        }
 
+        public function getAccountId() {
+            return $this -> accountId;
+        }
+
+        public function setAccountId($accountId) {
+            return $this -> accountId = $accountId;
+        }
+
+        public function getPointsNumber() {
+            return $this -> pointsNumber;
+        }
+
+        public function setPointsNumber($pointsNumber) {
+            return $this -> pointsNumber = $pointsNumber;
+        }
+
+        public function getPurchasesRecord() {
+            return $this -> purchasesRecord;
+        }
+
+        public function setPurchasesRecord($purchasesRecord) {
+            return $this -> purchasesRecord = $purchasesRecord;
         }
     };
 
     class Cart {
 
-        public $itemsQuantity;
+        protected $itemsQuantity;
 
         public function __construct($itemsQuantity) {
             $this -> itemsQuantity = $itemsQuantity;
 
         }
+
+        public function getItemsQuantity() {
+            return $this -> itemsQuantity;
+        }
+
+        public function setItemsQuantity($itemsQuantity) {
+            return $this -> itemsQuantity = $itemsQuantity;
+        }
     };
 
     class Wishlist extends Cart {
 
-        public $productsId;
+        protected $productId;
 
-        public function __construct($productsId, $itemsQuantity) {
-            $this -> productsId = $productsId;
+        public function __construct($productId, $itemsQuantity) {
+            $this -> productId = $productId;
 
             parent::__construct($itemsQuantity);
         }
+
+        public function getProductsId() {
+            return $this -> productId;
+        }
+
+        public function setProductId($productId) {
+            return $this -> productId = $productId;
+        }
     };
 
-    class Products {
+    class Product {
         
-        public $name;
-        public $description;
-        public $category;
-        public $price;
+        protected $name;
+        protected $description;
+        protected $category;
+        protected $price;
 
         public function __construct($name, $description, $category, $price) {
             $this -> name = $name;
@@ -70,28 +125,84 @@
             $this -> price = $price;
 
         }
+
+        public function getName() {
+            return $this -> name;
+        }
+
+        public function setName($name) {
+            return $this -> name = $name;
+        }
+
+        public function getDescription() {
+            return $this -> description;
+        }
+
+        public function setDescription($description) {
+            return $this -> description = $description;
+        }
+
+        public function getCategory() {
+            return $this -> category;
+        }
+
+        public function setCategory($category) {
+            return $this -> category = $category;
+        }
+
+        public function getPrice() {
+            return $this -> price;
+        }
+
+        public function setPrice($price) {
+            return $this -> price = $price;
+        }
     };
 
-    class SeasonalItems extends Products {
+    class SeasonalItems extends Product {
 
-        public $periodOfSale;
+        protected $periodOfSale;
 
         public function __construct($periodOfSale, $name, $description, $category, $price) {
             $this->periodOfSale = $periodOfSale;
             parent::__construct($name, $description, $category, $price);
         }
+
+        public function getPeriod() {
+            return $this -> periodOfSale;
+        }
+
+        public function setPeriod($periodOfSale) {
+            return $this -> periodOfSale = $periodOfSale;
+        }
         
     };
 
-    class DiscountedItems extends Products {
+    class DiscountedItems extends Product {
 
-        public $amountOfItems;
-        public $discountPercentage;
+        protected $amountOfItems;
+        protected $discountPercentage;
 
         public function __construct($amountOfItems, $discountPercentage, $name, $description, $category, $price) {
             $this->amountOfItems = $amountOfItems;
             $this->discountPercentage = $discountPercentage;
             parent::__construct($name, $description, $category, $price);
+        }
+
+        public function getAmountOfItems() {
+            return $this -> amountOfItems;
+        }
+
+        public function setAmountOfItems($amountOfItems) {
+            return $this -> amountOfItems = $amountOfItems;
+        }
+
+        public function getPeriod() {
+            return $this -> discountPercentage;
+        }
+
+        public function setPeriod($discountPercentage) {
+            return $this -> discountPercentage = $discountPercentage;
         }
     };
 
